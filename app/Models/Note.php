7f800a2dc211c9c6eb0f623560436ff8f430acba
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-05-04 14:20:00 -0300

    refactor

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -5,11 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Notes extends Model
+class Note extends Model
 {
     use HasFactory;
-    //NomeTabela
+
     protected $table = 'notes';
-        //Campos que aceitam dados em massa
-        protected $fillable = ['title', 'content', 'id_user'];
+        
+    /** 
+     * Campos que aceitam dados em massa
+    */
+    protected $fillable = ['title', 'content', 'id_user'];
 }
