2b4a58f3c6316c5c345f3d1c41bdc15c486cf99c
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2024-05-07 16:16:47 -0300

    fix (noteController): Arrumando toda a logica do controller

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -1,87 +1,88 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Note;
-use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
     //
-    public function store(Request $request){
-        $body = $request->all();
-        $data = [
-            'title' => '',
-            'content' => '',
-            'id_user' => ''
-        ];
-
-        if (empty($body['title'])) {
-            return response()->json(['error' => 'Title vazio.'], 400);
-        }
-        if (empty($body['content'])) {
-            return response()->json(['error' => 'Conteúdo vazio.'], 400);
-        }
-        if (empty($body['id_user'])) {
-            return response()->json(['error' => 'Id vazio.'], 400);
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+            'id_user' => 'required|exists:users,id' // Verifica a se o id existe na tabeça
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->first()], 400);
         }
 
-        $id_user = User::where('id', $body['id_user'])->first();
-        if ($id_user) {
-            $data['title'] = $body['title'];
-            $data['content'] = $body['content'];
-            $data['id_user'] = $body['id_user'];
+        try {
+            $note = new Note([
+                'title' => $request->title,
+                'content' => $request->content,
+                'id_user' => $request->id_user
+            ]);
 
-            Note::create($data);
-
+            $note->save();
             return response()->json(['success' => 'Anotação salva!.'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
         }
-        return response()->json(['error' => 'Id invalido.'], 400);
     }
 
-    public function read(Request $request, $id = null){
-        if($id == null){
-            return response()->json(['error' => 'Nenhum id inserido.'], 400);
-        }
-
-        $data = Note::where('id_user', $id)->get();
-
-        if (!$data->isEmpty()) {
-            return response()->json($data, 200);
-        }
-
-        return response()->json(['error' => 'ID errado ou nenhuma anotação.'], 400);
-    }
-
-    public function update(Request $request, $id = null){
-        if ($id === null) {
-            return response()->json(['error' => 'Nenhum ID inserido.'], 400);
-        }
-
-        $idVerify = Note::where('id', $id)->first();
-        if(!$idVerify){
+    public function read($id = null)
+    {
+        if (!is_numeric($id)) {
             return response()->json(['error' => 'Id invalido.'], 400);
         }
-
-        $Db = Note::findOrFail($id);
-        $Db->update($request->all());
-    
-        return response()->json($Db, 200);
+        try {
+            $notes = Note::where('id_user', $id)->get();
+            if ($notes->isEmpty()) {
+                return response()->json(['error' => 'Nenhuma anotação para o id fornecido.'], 400);
+            }
+            return response()->json($notes, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
-    public function delete($id){
-        if ($id === null) {
-            return response()->json(['error' => 'Nenhum ID inserido.'], 400);
+    public function update(Request $request, $id = null)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->first()], 400);
         }
-
-        $idVerify = Note::where('id', $id)->first();
-        if(!$idVerify){
-            return response()->json(['error' => 'Id invalido.'], 400);
+        try {
+            $idVerify = Note::where('id', $id)->first();
+            if (!$idVerify) {
+                return response()->json(['error' => 'Id invalido.'], 400);
+            }
+            $Db = Note::findOrFail($id);
+            $Db->update($request->all());
+            return response()->json($Db, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
         }
-
-        $idVerify->delete();
-        return response()->json(['success' => 'Nota apagada'], 400);
     }
 
-  
+    public function delete($id)
+    {
+        try {
+            $idVerify = Note::where('id', $id)->first();
+            if (!$idVerify) {
+                return response()->json(['error' => 'Id invalido.'], 400);
+            }
+            $idVerify->delete();
+            return response()->json(['success' => 'Nota apagada'], 400);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
